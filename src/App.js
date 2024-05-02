59d8a0b8ca6a30fb8f2e5b02ffb43a7cc22627df
LouayMagdy <magdylouay@gmail.com>
2024-05-02 23:20:49 +0300

    adding a dummy login page & authenticating user in the patientList page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
 } from 'react-router-dom';
 
 import MainLayout from './pages/main-layout/MainLayout'
+import Login from './pages/dummy-pages/Login';
+import Patients from './pages/patient-list/PatientList';
 
 import './App.css';
 
@@ -14,10 +16,12 @@ const App = () => {
   const router = createBrowserRouter(
       createRoutesFromElements(
           <Route path={'/'} element={<MainLayout/>}>
-            <Route index element={<div> home page </div>} />
-            {/*<Route path={'/about'} element={ <About/> } />*/}
+              <Route index element={<div> home page </div>} />
+              <Route path={'/dummy-login'} element={ <Login/> } />
+              <Route path={'/patients'} element={ <Patients/> } />
+              {/*<Route path={'/about'} element={ <About/> } />*/}
 
-            {/*<Route path={'*'} element={ <NotFound/> } />*/}
+              {/*<Route path={'*'} element={ <NotFound/> } />*/}
           </Route>
       )
   );
