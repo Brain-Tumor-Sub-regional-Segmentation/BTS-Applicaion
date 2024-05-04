c959334cd5acaf7df00e56bfe71bb537e7ded985
Habiba Osama <habebaosama14@gmail.com>
2024-05-04 05:39:41 +0300

    integrated with firebase

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,9 @@
 // SignUp.jsx
 import React, {useState, useEffect } from 'react';
 import'./SignUp.css'
+import db from '../../config/firebase-config';
+import bcrypt from 'bcryptjs';
+import { useNavigate } from 'react-router-dom';
 
 
 const SignUp = () => {
@@ -11,9 +14,13 @@ const SignUp = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [passwordError, setPasswordError] = useState('');
     const [phoneNumberError, setPhoneNumberError] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const navigate = useNavigate();
 
     // State for toggling password visibility
     const [showPassword, setShowPassword] = useState(false); 
+
 
     const handleTogglePasswordVisibility = () => {
         setShowPassword(!showPassword);
@@ -45,8 +52,10 @@ const SignUp = () => {
         return () => {
             registerLink.removeEventListener('click', handleRegisterClick);
             loginLink.removeEventListener('click', handleLoginClick);
+           
         };
     }, []);
+
     const resetInputFields = () => {
         setUsername('');
         setEmail('');
@@ -56,6 +65,20 @@ const SignUp = () => {
         setPasswordError('');
         setPhoneNumberError('');
     };
+
+
+    const hashPassword = async (password) => {
+        try {
+            const saltRounds = 10; // Number of salt rounds
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            return hashedPassword;
+        } catch (error) {
+            console.error('Error hashing password:', error);
+            return null;
+        }
+    };
+
+
 
     const handlePasswordChange = (e) => {
         const newPassword = e.target.value;
@@ -70,7 +93,7 @@ const SignUp = () => {
     };
 
     const validatePassword = (password) => {
-        console.log("password->", password);
+        //console.log("password->", password);
         // Regular expression for strong password
         const strongPasswordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
         if (!password) {
@@ -97,54 +120,137 @@ const SignUp = () => {
         }
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); // Prevent form submission
+    const validateEmail = (email) => {
+        // Regular expression for email validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      };
+      
+      // Function to handle email input change
+      const handleEmailChange = (e) => {
+        const newEmail = e.target.value;
+        setEmail(newEmail);
+        // Validate the new email format
+        if (!validateEmail(newEmail)) {
+          setEmailError('Please enter a valid email address.');
+        } else {
+          setEmailError('');
+        }
+      };
 
+      const handleSubmitLogin = async (e) => {
+        console.log("email>>>",email);
+        try {
+            // Check if email exists
+            const emailSnapshot = await db.collection('doctors').where('email', '==', email).get();
+    
+            if (emailSnapshot.empty) {
+                console.log('Email does not exist');
+                return { success: false, message: 'Email does not exist' };
+            }
+    
+            // Get the user document
+            const userDoc = emailSnapshot.docs[0];
+            const userData = userDoc.data();
+            console.log("3diiit")
+    
+            // Retrieve the hashed password from the user document
+            const hashedPassword = userData.password;
+    
+            // Compare the provided password with the hashed password
+            const passwordMatch = await bcrypt.compare(password, hashedPassword);
+    
+            if (passwordMatch) {
+                console.log('Login successful');
+                localStorage.setItem('id',userDoc.id);
+                navigate('/home');
+                return { success: true, message: 'Login successful' };
+
+            } else {
+                console.log('Incorrect password');
+                return { success: false, message: 'Incorrect password' };
+            }
+        } catch (error) {
+            console.error('Error logging in: ', error);
+            return { success: false, message: 'An error occurred while logging in' };
+        }
+    };
+    
+    const handleSubmitSignUp = async (e) => {
+        e.preventDefault(); // Prevent form submission
+    
         // Perform validation before submitting the form
         validatePassword(password);
         validatePhoneNumber(phoneNumber);
 
+        const hashedPassword = await hashPassword(password);
+    
         // If there are no errors, you can proceed with form submission
         if (!passwordError && !phoneNumberError) {
-            // Perform form submission logic here
-            console.log('Form submitted successfully');
-            //and route to home page
+            try {
+                // Check if email already exists
+                const emailSnapshot = await db.collection('doctors').where('email', '==', email).get();
+                
+                // If email exists, display an error message
+                if (!emailSnapshot.empty) {
+                    console.log('Email already exists');
+                    return; // Exit the function
+                }
+    
+                // If email doesn't exist, proceed with signup
+                const docRef = await db.collection('doctors').add({
+                    name: username,
+                    email: email,
+                    phone: phoneNumber,
+                    doctorId: doctorId,
+                    password: hashedPassword
+                });
+    
+                // Get the ID of the newly created document
+                const newDocId = docRef.id;
+                console.log('New doctor added with ID:', newDocId);
+    
+                // Reset input fields
+                resetInputFields();
+                localStorage.setItem('id',newDocId);
+                navigate('/home');
+    
+                // Handle navigation or display success message
+            } catch (error) {
+                console.error('Error adding doctor: ', error);
+                // Handle error (e.g., display error message to user)
+            }
         } else {
             console.log('Form has errors, cannot submit.');
         }
-    
     };
+    
 
-
+  
     return (
         <>
             <div className="wrapper">
-
                 <span className="rotate-bg"></span>
                 <span className="rotate-bg2"></span>
 
                 <div className="form-box login">
-
                     <h2 className="title animation" style={{ '--i': 0, '--j': 21 , color: "#002844" }}>Login</h2>
 
-                    <form action="#">
+                    <form action="#" onSubmit={handleSubmitLogin}>
 
-                        <div className="input-box animation" style={{ '--i': 1, '--j': 22 }}>
-                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                            <label htmlFor="">Username</label>
-                            <i className='bx bxs-user'></i>
-                        </div>
-
-                
+                         <div className="input-box animation" style={{ '--i': 19, '--j': 2 }}>
+                         <input type="email" value={email} onChange={handleEmailChange} required />
+                          <label htmlFor="">Email</label>
+                          <i className='bx bxs-envelope'></i>
+                          {emailError && <span className="error-message">{emailError}</span>}
+                           </div>
 
                         <div className="input-box animation" style={{ '--i': 2, '--j': 23 }}>
                         <input type="password" value={password} onChange={handlePasswordChange} required />
                         <label htmlFor="">Password</label>
-                           {/* <i className='bx bxs-lock-alt'></i>*/}
-                              {/* Toggle password visibility icon */}
                         <span className="toggle-password" onClick={handleTogglePasswordVisibility}>
-                     <i className={showPassword ? 'bx bxs-hide' : 'bx bxs-show'}></i>
-                  </span>
+                       <i className={showPassword ? 'bx bxs-hide' : 'bx bxs-show'}></i>
+                       </span>
                         </div>
 
                         <button type="submit" className="btn animation" style={{ '--i': 3, '--j': 24 }}>Login</button>
@@ -165,7 +271,7 @@ const SignUp = () => {
 
                     <h2 className="title animation" style={{ '--i': 17, '--j': 0 , color: "#002844"}}>Sign Up</h2>
 
-                    <form action="#" onSubmit={handleSubmit}>
+                    <form action="#" onSubmit={handleSubmitSignUp}>
 
                         <div className="input-box animation" style={{ '--i': 18, '--j': 1 }}>
                         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
@@ -173,14 +279,16 @@ const SignUp = () => {
                             <i className='bx bxs-user'></i>
                         </div>
 
+                        
                         <div className="input-box animation" style={{ '--i': 19, '--j': 2 }}>
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                            <label htmlFor="">Email</label>
-                            <i className='bx bxs-envelope'></i>
-                        </div>
+                         <input type="email" value={email} onChange={handleEmailChange} required />
+                          <label htmlFor="">Email</label>
+                          <i className='bx bxs-envelope'></i>
+                          {emailError && <span className="error-message">{emailError}</span>}
+                           </div>
 
                        <div className="input-box animation" style={{ '--i': 21, '--j': 4 }}>
-                               <input type="tel" onChange={handlePhoneNumberChange} required />
+                               <input type="tel" value ={phoneNumber} onChange={handlePhoneNumberChange} required />
                                <label htmlFor="">Phone Number</label>
                                <i className='bx bxs-phone'></i>
                                {phoneNumberError && <span className="error-message">{phoneNumberError}</span>}
@@ -192,7 +300,7 @@ const SignUp = () => {
                             <i className='bx bxs-id-card'></i>
                         </div> 
                         <div className="input-box animation" style={{ '--i': 20, '--j': 3 }}>
-                        <input type={showPassword ? 'text' : 'password'} onChange={handlePasswordChange} required />
+                        <input type={showPassword ? 'text' : 'password'}  value={password} onChange={handlePasswordChange} required />
                             <label htmlFor="">Password</label>
                             {/*<i className='bx bxs-lock-alt'></i>*/}
                             <div className="error-message-container">
