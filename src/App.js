6eb3952dcf6b5abd37ff8c549291d191c3e1592a
BaselAhmed <basel20ahmed@gmail.com>
2024-05-04 14:08:47 +0300

    finished ui of landing and new patient pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import NewPatient from "./pages/NewPatient"
+import Dummy from "./Dummy";
 
 function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/landingpage":
+        title = "BTS";
+        metaDescription = "Brain Tumor Segmentation";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  // if (pathname === "/")
+  //   return  <Navigate to="/landingpage"></Navigate>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/logIn" element={<Dummy/>} />
+      <Route path="/signUp" element={<Dummy/>} />
+      <Route path="/newPatient" element={<NewPatient />} />
+    </Routes>
   );
 }
-
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,53 @@
+import styles from "./NavBar.module.css";
+import SettingsMenu from "./SettingsMenu";
+
+const NavBar = ({firstLetter}) => {
+  return (
+    <section className={styles.patient}>
+      <div className={styles.frameParent}>
+        <div className={styles.frame}>
+          <div className={styles.groupWrapper}>
+            <img
+              className={styles.groupIcon}
+              loading="lazy"
+              alt=""
+              src="/group.svg"
+            />
+          </div>
+          <img
+            className={styles.groupIcon1}
+            loading="lazy"
+            alt=""
+            src="/group-1.svg"
+          />
+          <img
+            className={styles.groupIcon2}
+            loading="lazy"
+            alt=""
+            src="/group-2.svg"
+          />
+          <img className={styles.vectorIcon} alt="" />
+        </div>
+        <div className={styles.frameWrapper}>
+          <div className={styles.frameGroup}>
+            <div className={styles.homeParent} onClick={() => console.log("click")}>
+              <div className={styles.home}>Home</div>
+              <div className={styles.lineWrapper}>
+                <div className={styles.frameChild} />
+              </div>
+            </div>
+            <div className={styles.processParent} onClick={() => console.log("click")}>
+              <div className={styles.process}>Process</div>
+              <div className={styles.lineContainer}>
+                <div className={styles.frameItem} />
+              </div>
+            </div>
+            <SettingsMenu firstLetter={firstLetter}/>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NavBar;
