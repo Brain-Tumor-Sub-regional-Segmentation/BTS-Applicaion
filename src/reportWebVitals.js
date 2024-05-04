30fcb9ca2aa553955d5638863f416ce31afdde95
BaselAhmed <basel20ahmed@gmail.com>
2024-05-04 14:47:06 +0300

    successfully rebased remote branch

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-=======
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
->>>>>>> 3c70ccb (Initialize project using Create React App)
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
